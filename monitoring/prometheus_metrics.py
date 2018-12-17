93005a69593568e0b2fa477e1e4541ee8185678e
Aparna Karve <akarve@redhat.com>
2018-12-17 15:40:50 -0800

    fix the labels

--- monitoring/prometheus_metrics.py
+++ monitoring/prometheus_metrics.py
@@ -14,18 +14,18 @@ data_collector_jobs_denied = Counter(   #noqa
     'The total number of denied data collector jobs'
 )
 data_download_requests_total = Counter(   #noqa
-    'data_download_inprogress_requests',
+    'data_download_requests_total',
     'The total number of data download inprogress requests'
 )
 data_download_request_exceptions = Counter(   #noqa
-    'data_download_inprogress_request_exceptions',
+    'data_download_request_exceptions',
     'The total number of data download inprogress request exceptions'
 )
 post_data_requests_total = Counter(   #noqa
-    'post_data_inprogress_requests',
+    'post_data_requests_total',
     'The total number of post data inprogress requests'
 )
 post_data_request_exceptions = Counter(   #noqa
-    'post_data_inprogress_request_exceptions',
+    'post_data_request_exceptions',
     'The total number of post data inprogress request exceptions'
 )
