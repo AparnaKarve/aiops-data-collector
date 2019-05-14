68d53bacdaea5409113eee79259f1a1e9809e787
Aparna Karve <akarve@redhat.com>
2019-05-14 13:35:57 -0700

    set metric only when size > 0

--- collector/topological_inventory.py
+++ collector/topological_inventory.py
@@ -237,7 +237,8 @@ def worker(_: str, source_id: str, dest: str, acct_info: dict) -> None:
                 data_size = \
                     topological_inventory_data(_, source_id, dest,
                                                headers, thread)
-                prometheus_metrics.METRICS['data_size'].set(data_size)
+                if data_size > 0:
+                    prometheus_metrics.METRICS['data_size'].set(data_size)
                 utils.set_processed(tenant_header['acct_no'])
                 LOGGER.debug('%s: ---END Account# %s---',
                              thread.name, tenant_header['acct_no'])
