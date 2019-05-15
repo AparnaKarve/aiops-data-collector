7c180d2b54f5faad734aa64921510d0dc6f4fc6e
Aparna Karve <akarve@redhat.com>
2019-05-15 09:33:15 -0700

    more time adjustments

--- collector/topological_inventory.py
+++ collector/topological_inventory.py
@@ -220,16 +220,16 @@ def worker(_: str, source_id: str, dest: str, acct_info: dict) -> None:
 
     today = datetime.today().strftime('%Y%m%d')
 
-    if ALL_TENANTS:
-        prometheus_metrics.METRICS['gets'].inc()
-        resp = utils.retryable('get', TENANTS_URL, headers=headers)
-        prometheus_metrics.METRICS['get_successes'].inc()
-        tenants_headers = \
-            [tenant_header_info(t["external_tenant"]) for t in resp.json()]
+    with DATA_COLLECTION_TIME.time():
+        if ALL_TENANTS:
+            prometheus_metrics.METRICS['gets'].inc()
+            resp = utils.retryable('get', TENANTS_URL, headers=headers)
+            prometheus_metrics.METRICS['get_successes'].inc()
+            tenants_headers = \
+                [tenant_header_info(t["external_tenant"]) for t in resp.json()]
 
-        LOGGER.info('Fetching data for ALL(%s) Tenants', len(tenants_headers))
+            LOGGER.info('Fetching data for ALL(%s) Tenants', len(tenants_headers))
 
-        with DATA_COLLECTION_TIME.time():
             for tenant_header in tenants_headers:
                 LOGGER.debug('%s: ---START Account# %s---',
                          thread.name, tenant_header['acct_no'])
@@ -246,9 +246,8 @@ def worker(_: str, source_id: str, dest: str, acct_info: dict) -> None:
                 utils.set_processed(tenant_header['acct_no'])
                 LOGGER.debug('%s: ---END Account# %s---',
                                     thread.name, tenant_header['acct_no'])
-    else:
-        LOGGER.info('Fetching data for current Tenant')
-        with DATA_COLLECTION_TIME.time():
+        else:
+            LOGGER.info('Fetching data for current Tenant')
             data_size = \
                 topological_inventory_data(_, source_id, dest, headers, thread)
             if data_size > 0:
@@ -331,6 +330,7 @@ def topological_inventory_data(
             )
             return 0.0
 
+    print(f"JSON size is {get_deep_size(data)}")
     # Pass to next service
     prometheus_metrics.METRICS['posts'].inc()
     try:
