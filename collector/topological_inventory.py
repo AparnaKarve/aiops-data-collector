6e058a5a4a040fd2159f39e1c448cb7177c51a5f
Aparna Karve <akarve@redhat.com>
2019-05-15 20:52:25 -0700

    Revert "gauge random"
    This reverts commit c7d8f21b97bd52d4fb27408be03d101ec1afff16.

--- collector/topological_inventory.py
+++ collector/topological_inventory.py
@@ -238,9 +238,7 @@ def worker(_: str, source_id: str, dest: str, acct_info: dict) -> None:
                 data_size = \
                     topological_inventory_data(_, source_id, dest,
                                            headers, thread)
-                prometheus_metrics.METRICS['data_size'].inc(data_size)
-
-                prometheus_metrics.METRICS['data_size'].dec(100)
+                prometheus_metrics.METRICS['data_size'].set(data_size)
 
                 if data_size > 0:
                     prometheus_metrics.METRICS['data_size_above_ceiling'].labels(
